faf49c9afda879ef39c5b10b23b0749c38f243c9
Dheeraj Kumar <dheeraj2525dk@gmail.com>
2026-09-11 22:36:43 +0530

    changes done in index

--- backend/db.js
+++ backend/db.js
@@ -3,13 +3,13 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "uvesms",   // ← your DB name
+  database: "UVESMS",   // ← your DB name
   password: "1234", // change if needed
   port: 5432,
 });
 
 pool.connect()
-  .then(() => console.log("✅ Connected to PostgreSQL (uvesms)"))
+  .then(() => console.log("✅ Connected to PostgreSQL (UVESMS)"))
   .catch(err => {
     console.error("❌ DB connection failed:", err.message);
     process.exit(1);
